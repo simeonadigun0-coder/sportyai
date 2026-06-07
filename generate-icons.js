42eb11291e6918db0e376113cbccd0d9439f0e75
Simeon <simeonadigun0@email.com>
2026-06-07 08:37:45 +0100

    fix PWA icon background color

--- generate-icons.js
+++ generate-icons.js
@@ -1,5 +1,5 @@
 const sharp = require('sharp');
 const sizes = [72, 96, 128, 192, 512];
 Promise.all(sizes.map(s =>
-  sharp('public/logo.png').resize(s, s, { fit: 'contain', background: { r: 26, g: 61, b: 30, alpha: 1 } }).png().toFile('public/icons/icon-' + s + '.png')
+  sharp('public/logo.png').resize(s, s, { fit: 'contain', background: { r: 13, g: 31, b: 14, alpha: 1 } }).png().toFile('public/icons/icon-' + s + '.png')
 )).then(() => console.log('Done!')).catch(e => console.log(e.message));
