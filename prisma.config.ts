cc1f457f7388e0cc9289bf57b4933548aaf7f66b
Simeon <simeonadigun0@email.com>
2026-06-28 22:33:45 +0100

    fix: prisma config datasource property name

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
-  datasourceUrl: "postgresql://postgres.hhoxyuuxfopvjwyumftu:@Olalekan1996@aws-0-eu-west-1.pooler.supabase.com:6543/postgres",
   schema: 'prisma/schema.prisma',
+  datasource: {
+    url: process.env.DATABASE_URL as string,
+  },
 })
